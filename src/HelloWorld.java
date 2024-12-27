e3c13c26bc0b6cbf3d25c042856a237b4a8d51ab
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-12-27 08:37:35 +0600

    added comment to HelloWorld program

--- src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    static void printHelloWorld() {
-        System.out.println("Hello World");
-    }
-
-    public static void main(String[] args) {
-        printHelloWorld();
-    }
-}
---
+++ src/module1/HelloWorld.java
@@ -0,0 +1,27 @@
+package module1;
+
+/**
+ * A simple class to print "Hello World" to the console.
+ *
+ * @author Sheikh Md. Sazidul Islam
+ * @since 27/12/2024
+ * @version 3.0
+ * @see <a href="https://github.com/sazid99246">My Github</a>
+ */
+public class HelloWorld {
+    /**
+     * Prints the message "Hello World" to the console.
+     */
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * The main method, serving as the entry point of the program.
+     *
+     * @param args Command-line arguments (not used in this program).
+     */
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}
